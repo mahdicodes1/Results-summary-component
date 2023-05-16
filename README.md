5b2785bca51df264ddc3a1fe026b431e12c4d1a8
Mahdi Ali <mahid.amke@gmail.com>
2023-05-16 15:01:04 +0300

    Release1

---
+++ README.md
@@ -0,0 +1,53 @@
+ 
+# A challenge on [frontendmentor](https://www.frontendmentor.io)
+ 
+
+<img src="./design/active-states.jpg">
+<br>
+<br>
+
+# **Challenge Name**
+
+- ### [Results summary component](https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa)
+
+<br>
+
+# **Description**
+This component shows a result and summary of a cognitive ability test.  
+The data, which consists of summary, and some CSS properties for styling them, is saved in the data.json file. After loading data from this file into the DOM, the result will calculate too.  
+Then, with scrollreveal.js, information will show to the user.
+For using this component, an internet connection is needed.
+ 
+
+## **Layout**
+
+The designs were created to the following widths:
+
+- Mobile: 493px
+- Desktop: 1440px
+<br>
+
+## **Links**
+
+- Live Site URL: [click here](https://mahdicodes1.github.io/Product-preview-card-component/)
+
+<br>
+
+## **Built With**
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) &nbsp;  ![CSS3](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)&nbsp;
+![ScrollReveal](https://img.shields.io/badge/ScrollReveal%20js-0e141f?style=for-the-badge&logo=ScrollReveal&logoColor=e39ff6)&nbsp;
+![jQuery](https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white)
+- Family: [Hanken Grotesk](https://fonts.google.com/specimen/Hanken+Grotesk)
+
+<br>
+
+## **Tools Used**
+
+![Visual Studio Code](https://img.shields.io/badge/VS%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+</br>
+
+## **Acknowledgment**
+
+- I'd like to thank [Frontend Mentor](https://www.frontendmentor.io) for this challenge.
---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+
+$(document).ready(function () { 
+  // FETCHING DATA FROM JSON FILE
+  $.getJSON("./assets/data/data.json", function (data) {
+    var test = "";
+    var total = 0; // For calculating the total that will use to calculate the result.
+    var result = 0; // For calculating the average, which is called: result.
+
+    $.each(data, function (key, value) {
+      var testCategory = value.category; // Category value in the data.json file
+      var iconSrc = value.icon; // icon value in the data.json file (sorce of icon)
+      var backgroundClrTest = value.background; // background color value in the data.json file
+      var colorTest = value.color; // color value in the data.json file
+      test += "<tr>";
+      test +=
+        "<td id='" +
+        testCategory +
+        "'> <div class='test-name'>" +
+        "<img src=" +
+        iconSrc +
+        ">" +
+        value.category +
+        "</div>" +
+        "<div class='test-score'>" +
+        value.score +
+        "<span> / 100 </span>" +
+        " </div></td>";
+
+      test += "</tr>";
+      $("#ability-test").append(test);
+
+      $("#" + testCategory).css("background", backgroundClrTest);
+      $("#" + testCategory + " .test-name").css("color", colorTest);
+
+      total += value.score;
+      test = "";
+    });
+
+    result = Math.round(total / 4); // Calculate the average, which is called: result.
+    const resultText = document.querySelector(".result-pane .average-result p");
+    $(resultText).text(result);
+
+    //  Codes for animation 
+
+    // Animates result
+    ScrollReveal().reveal(resultText, {
+      distance: "150%",
+      delay: 500,
+      duration: 1000,
+    });
+    // Animates the status of the result that is Great!
+    ScrollReveal().reveal(".result-description > h3", {
+      distance: "150%",
+      delay: 1000,
+      duration: 1000,
+    });
+    // Animates the description of the result.
+    ScrollReveal().reveal(".result-description > p", {
+      distance: "150%",
+      delay: 2000,
+      duration: 1000,
+    });
+
+    const productDescription = document.querySelector("tbody"); // This variable saves all tr rows that contain data about the test.
+
+    for (const child of productDescription.children) {
+      child.classList.add("reveal-top");
+    }
+    // Animates test data
+    ScrollReveal().reveal(".reveal-top", {
+      distance: "150%",
+      delay: 5000,
+      duration: 1000,
+      interval: 1750,
+    });
+    // Animates the button: Continue
+    ScrollReveal().reveal(".summary-pane > button", {
+      opacity: 0,
+      delay: 12000,
+      duration: 1000,
+    });
+  });
+});
